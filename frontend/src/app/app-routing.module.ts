fb53ffcc8cf4514006f56599c22f285c3efda6b3
Workman, Jayson <jworkman1@metlife.com>
2021-05-25 14:58:20 -0400

    login inprogress

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'auth', 
+    children: [
+      { path: 'login', component: LoginComponent},
+      { path: '**', redirectTo: '/auth/login', pathMatch: 'full'}
+    ]
+  },
+  {path: '', redirectTo: '/auth/login', pathMatch: 'full'},
+  {path: '**', redirectTo: '/auth/login', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
