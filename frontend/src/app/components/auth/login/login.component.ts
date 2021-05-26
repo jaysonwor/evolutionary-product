14a65bab68e9492cd0786f910b13486b90c3a8c3
Workman, Jayson <jworkman1@metlife.com>
2021-05-26 15:06:29 -0400

    login form done

--- frontend/src/app/components/auth/login/login.component.ts
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Optional } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: any;
+  isSubmitting: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
+
+  initForm() {
+    this.form = this.formBuilder.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required]
+    })
+  }
+
+  get f() { return this.form.controls; }
+
+  login() {
+    console.log("LoginComponent.login")
+    this.isSubmitting = true;
+    //todo add api call
+    // this.isSubmitting = false;
+  }
+
 
 }
--- frontend/src/app/ngmaterial.module.ts
+++ frontend/src/app/ngmaterial.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
